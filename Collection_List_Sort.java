14968640f113b39c62da08e33652bc34c474ab29
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-07-09 16:16:29 +0530

    Create Collection_List_Sort.java

---
+++ Collection_List_Sort.java
@@ -0,0 +1,34 @@
+
+import java.util.*;  
+  
+class Student implements Comparable<Student> {  
+    public String name;  
+  public Student(String name) {  
+    this.name = name;  
+  }  
+  public int compareTo(Student person) {  
+    return name.compareTo(person.name);  
+      
+  }  
+}  
+public class Collection_List_Sort {  
+  public static void main(String[] args) {  
+      ArrayList<Student> al=new ArrayList<Student>();  
+      al.add(new Student("Viru"));  
+      al.add(new Student("Saurav"));  
+      al.add(new Student("Mukesh"));  
+      al.add(new Student("Tahir"));  
+      
+    Collections.sort(al);  
+    for (Student s : al) {  
+      System.out.println(s.name);  
+    }  
+  }  
+}
+//Output
+/*
+Mukesh
+Saurav
+Tahir
+Viru
+*/
